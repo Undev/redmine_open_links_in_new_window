a2f33dde08c97b9c2bc2028c8c53be67c475953c
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2011-03-02 15:38:05 +0300

    Open all links on issue description in new window too

--- assets/javascripts/redmine_attachments_in_new_window.js
+++ assets/javascripts/redmine_attachments_in_new_window.js
@@ -2,7 +2,7 @@ document.observe('dom:loaded', function()
 {
 	// redmine uses prototype so use it.
 
-	var links = $$('a.icon-attachment');
+	var links = $$('div.issue.details div.wiki a, div.issue.details div.attachments a');
 	for (var i = 0; i < links.length; i++)
 	{
 		var link = links[i];
