01dc3fa91f79560842bee68c2c3692911b9064f4
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2011-03-04 14:50:00 +0300

    Typo

--- lib/redmine_open_links_in_new_window/view_layouts_base_html_head_hook.rb
+++ lib/redmine_open_links_in_new_window/view_layouts_base_html_head_hook.rb
@@ -1,7 +1,7 @@
 module RedmineOpenLinksInNewWindow
   class ViewLayoutsBaseHtmlHeadHook < Redmine::Hook::ViewListener
     def view_layouts_base_html_head(context = {})
-      javascript_include_tag('open_links_in_new_window.js', :plugin => :open_links_in_new_window)
+      javascript_include_tag('open_links_in_new_window.js', :plugin => :redmine_open_links_in_new_window)
     end
   end
 end
