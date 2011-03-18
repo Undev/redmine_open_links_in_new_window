b9444ed7d92c8968d16f898631edf4ea08935982
Florian Berninger <florian.berninger@kwick.de>
2011-03-18 08:39:27 +0100

    Added a.external, used in tickets e.g.

--- assets/javascripts/redmine_open_links_in_new_window.js
+++ assets/javascripts/redmine_open_links_in_new_window.js
@@ -2,7 +2,7 @@ document.observe('dom:loaded', function()
 {
 	// redmine uses prototype so use it.
 
-	var links = $$('div.wiki a, div.attachments a, div.journal ul.details a');
+	var links = $$('div.wiki a, div.attachments a, div.journal ul.details a a.external');
 	for (var i = 0; i < links.length; i++)
 	{
 		links[i].target = '_blank';
