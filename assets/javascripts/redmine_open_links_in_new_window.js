40f0214b4584b4ccb48eaeb326cd216c0cc97099
And <logical_and@lavabit.com>
2012-10-01 21:59:22 +0300

    Now work on Redmine 2.1.0.

--- assets/javascripts/redmine_open_links_in_new_window.js
+++ assets/javascripts/redmine_open_links_in_new_window.js
@@ -1,11 +1,31 @@
-document.observe('dom:loaded', function()
+if (document.observe)
 {
+
 	// redmine uses prototype so use it.
 
-	var links = $$('div.wiki a, div.attachments a, div.journal ul.details a, a.external');
-	for (var i = 0; i < links.length; i++)
+	document.observe('dom:loaded', function()
 	{
-		links[i].target = '_blank';
-	}
-});
+		var links = $$('div.wiki a, div.attachments a, div.journal ul.details a, a.external');
+		for (var i = 0; i < links.length; i++)
+		{
+			links[i].target = '_blank';
+		}
+	});
+}
+else if (window.jQuery)
+{
 
+	// redmine uses jQuery so use it.
+
+	$(document).ready(function()
+	{
+		$$('div.wiki a, div.attachments a, div.journal ul.details a, a.external').each(function()
+		{
+			this.target = '_blank';
+		});
+	});
+}
+else
+{
+	console.error('(redmine_open_links_in_new_window) JS-framework is unknown!');
+}
