ed74e25f3071034c01e587cf4dd189682047510b
And <logical_and@lavabit.com>
2012-10-01 22:14:54 +0300

    Open elements in external window disabled feature disabled on wiki anchor links (#page-anchor).

--- assets/javascripts/redmine_open_links_in_new_window.js
+++ assets/javascripts/redmine_open_links_in_new_window.js
@@ -1,31 +1,47 @@
-if (document.observe)
+(function()
 {
-
-	// redmine uses prototype so use it.
-
-	document.observe('dom:loaded', function()
+	/**
+	 * Need to be binded to anchor on call
+	 */
+	var handleAnchor = function()
 	{
-		var links = $$('div.wiki a, div.attachments a, div.journal ul.details a, a.external');
-		for (var i = 0; i < links.length; i++)
-		{
-			links[i].target = '_blank';
-		}
-	});
-}
-else if (window.jQuery)
-{
+		var pass = true;
 
-	// redmine uses jQuery so use it.
+		// Requirements
+		if (-1 != this.className.indexOf('wiki-anchor')) pass = false;
 
-	$(document).ready(function()
-	{
-		$$('div.wiki a, div.attachments a, div.journal ul.details a, a.external').each(function()
+		if (pass)
 		{
 			this.target = '_blank';
+		}
+	};
+
+	if (document.observe)
+	{
+
+		// redmine uses prototype so use it.
+
+		document.observe('dom:loaded', function()
+		{
+			var links = $$('div.wiki a, div.attachments a, div.journal ul.details a, a.external');
+			for (var i = 0; i < links.length; i++)
+			{
+				handleAnchor.call(links[i]);
+			}
 		});
-	});
-}
-else
-{
-	console.error('(redmine_open_links_in_new_window) JS-framework is unknown!');
-}
+	}
+	else if (window.jQuery)
+	{
+
+		// redmine uses jQuery so use it.
+
+		jQuery(document).ready(function()
+		{
+			jQuery('div.wiki a, div.attachments a, div.journal ul.details a, a.external').each(handleAnchor);
+		});
+	}
+	else
+	{
+		console.error('(redmine_open_links_in_new_window) JS-framework is unknown!');
+	}
+})();
