ef047bb3d65cd6c3a39cf5511522b348e7536c01
Irina Lebedevich <irina.lebedevich@undev.ru>
2015-01-16 13:33:27 +0300

    ref #56814 Init.rb updated

--- init.rb
+++ init.rb
@@ -3,9 +3,9 @@ require 'redmine'
 require_dependency 'redmine_open_links_in_new_window/view_layouts_base_html_head_hook'
 
 Redmine::Plugin.register :redmine_open_links_in_new_window do
-  name 'Redmine Open Links In New Window plugin'
-  author 'akzhan.abdulin@gmail.com'
-  description 'This is the Open Links In New Window plugin for Redmine'
+  name 'Redmine Plugin for Opening Links in New Tabs'
+  author 'Undev'
+  description 'This plugin opens links in a new browser tab instead of the current tab.'
   version '0.0.3'
   url 'http://github.com/Undev/redmine_open_links_in_new_window'
   author_url 'http://github.com/Undev'
